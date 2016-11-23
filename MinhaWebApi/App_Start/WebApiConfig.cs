d5dddca60b58c73f91c2a30eff1810e73c24b37d
Ezequiel Bertti <ebertti@gmail.com>
2016-11-22 22:53:29 -0200

    inicial

--- MinhaWebApi/App_Start/WebApiConfig.cs
+++ MinhaWebApi/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Swashbuckle.Application;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -10,6 +11,7 @@ namespace MinhaWebApi
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableSwagger(c => c.SingleApiVersion("v1", "Minha Super API")).EnableSwaggerUi();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
